7c68a4d4e5c56d4126e4b909b4deeef17c3b9034
Abdallah Hassan <engabdallahhassan6@gmail.com>
2025-09-26 09:47:46 +0300

    after finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,36 +7,52 @@ export default function Home() {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const endDate = new Date("Feb 25, 2026").getTime();
+  const [finished, setFinished] = useState(false); // after the time is over, change the default value to "true"
+  const date = "Jun 1, 2025";
+  const endDate = new Date(date).getTime();
 
   // Update the count down every 1 second
-  const interval = setInterval(function () {
+  setInterval(function () {
     // Get today's date and time
     const now = new Date().getTime();
 
     // Find the distance between now and the count down date
-    const distance = endDate - now;
+    let distance = endDate - now;
+
+    //Check if the time is over or not
+    if (!finished && distance < 0) {
+      setFinished(true);
+    }
+
+    distance = distance < 0 ? -distance : distance;
 
     // Time calculations for days, hours, minutes and seconds
     setDays(Math.floor(distance / (1000 * 60 * 60 * 24)));
     setHours(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     setMinutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
     setSeconds(Math.floor((distance % (1000 * 60)) / 1000));
-
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(interval);
-    }
   }, 1000);
+
+  let pageStyle =
+    "h-screen flex flex-col bg-[url('/download.jpg')] bg-no-repeat bg-cover";
+  if (finished)
+    pageStyle =
+      "h-screen flex flex-col bg-[url('/ministry.png')] bg-no-repeat bg-cover";
+      
   return (
-    <div className="h-screen flex flex-col bg-[url('/download.jpg')] bg-no-repeat bg-cover">
+    <div className={pageStyle}>
       <div className="text-center text-white text-4xl font-semibold p-8 font-serif mt-10">
         Ministry of Interior
         <div className="mt-20 -mb-50">
-          End date: <span className=" text-red-500">February 25, 2026</span>
+          End date: <span className=" text-red-500">{date}</span>
         </div>
       </div>
-      <div className="flex-grow flex flex-row flex-wrap items-center justify-center">
+      {finished && (
+        <div className="text-center text-white text-4xl font-semibold p-8 font-serif mt-30 -mb-50">
+          It &apos; s over since
+        </div>
+      )}
+      <div className="flex flex-row flex-wrap items-center justify-center mt-60">
         <Time number={days} type={"days"} />
         <Time number={hours} type={"hours"} />
         <Time number={minutes} type={"minutes"} />
